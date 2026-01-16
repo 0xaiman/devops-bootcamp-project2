93f8d62a65d7c576b80458203ee59d05f1533b17
0xaiman <aimandani9797@gmail.com>
2026-01-16 22:46:55 +0800

    feat: store ansible .ini and .cfg in s3 then pull via cloud-init

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -16,3 +16,21 @@ resource "aws_s3_object" "pkey" {
 
   depends_on = [local_file.ansible_private_key]
 }
+
+resource "aws_s3_object" "inventory_ini" {
+  bucket = aws_s3_bucket.bootstrap_bucket.id
+  key    = "inventory.ini"
+  source = "${path.module}/../ansible/inventory.ini"
+  etag   = filemd5("${path.module}/../ansible/inventory.ini")
+
+  depends_on = [local_file.ansible_private_key]
+}
+
+resource "aws_s3_object" "ansible_cfg" {
+  bucket = aws_s3_bucket.bootstrap_bucket.id
+  key    = "ansible.cfg"
+  source = "${path.module}/../ansible/ansible.cfg"
+  etag   = filemd5("${path.module}/../ansible/ansible.cfg")
+
+  depends_on = [local_file.ansible_private_key]
+}
