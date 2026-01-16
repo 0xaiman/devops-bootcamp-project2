809acab6faf0152605ec0efc2de86a510d262735
0xaiman <aimandani9797@gmail.com>
2026-01-17 02:41:24 +0800

    fix

--- terraform/key_push.tf
+++ terraform/key_push.tf
@@ -1,15 +1,23 @@
-resource "aws_ssm_association" "upload_pem" {
-  name = "AWS-RunShellScript"
+# resource "aws_ssm_association" "upload_pem" {
+#   name = "AWS-RunShellScript"
 
-  targets {
-    key    = "InstanceIds"
-    values = [aws_instance.controller.id]
-  }
+#   targets {
+#     key    = "InstanceIds"
+#     values = [aws_instance.controller.id]
+#   }
 
-  parameters = {
-    commands = "aws s3 cp ./pkey.pem s3://bucket-bootstrapping-devops-2/pkey.pem"
-  }
+#   parameters = {
+#     commands = "aws s3 cp ./pkey.pem s3://bucket-bootstrapping-devops-2/pkey.pem"
+#   }
 
+#   depends_on = [local_file.ansible_private_key]
+
+# }
+
+resource "null_resource" "upload_pem_to_s3" {
   depends_on = [local_file.ansible_private_key]
 
+  provisioner "local-exec" {
+    command = "aws s3 cp ./pkey.pem s3://bucket-bootstrapping-devops-2/pkey.pem"
+  }
 }
